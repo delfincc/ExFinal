6a2fbddb57f25548f57a0a1e99b2a8ce4216b20b
delfincc <139388427+delfincc@users.noreply.github.com>
2023-07-18 12:10:07 -0500

    commited

--- app/src/main/java/com/example/exfinal/HomeActivity.java
+++ app/src/main/java/com/example/exfinal/HomeActivity.java
@@ -1,10 +1,15 @@
 package com.example.exfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
+    FragmentTransaction transaction;
+    BoletaFragment boleta;
+    ProductosFragment productos;
+    ElijaFragment elija;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
